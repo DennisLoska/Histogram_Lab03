ba0a206b19fc7783d07959e83c8f384c55bf621b
DennisTalmid <d.loska@hotmail.de>
2017-04-24 20:36:52 +0200

    init lab03 histogram

---
+++ Histogram/src/HistogramSystem.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Dennis on 24.04.2017.
+ */
+public class HistogramSystem {
+    public static void main(String[] args){
+        System.out.println("Test");
+    }
+
+}
---
+++ Histogram/src/Test/HistogramTest.java
@@ -0,0 +1,7 @@
+package Test;
+
+/**
+ * Created by Dennis on 24.04.2017.
+ */
+public class HistogramTest {
+}
