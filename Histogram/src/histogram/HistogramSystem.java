dd1ec8264ad464286c208f94c5c56239d1af0d4d
disrupted <salomon.popp@gmail.com>
2017-04-26 13:40:40 +0200

    read test file

--- Histogram/src/histogram/HistogramSystem.java
+++ Histogram/src/histogram/HistogramSystem.java
@@ -1,11 +1,35 @@
 package histogram;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
 /**
  * Created by Dennis on 24.04.2017.
  */
 public class HistogramSystem {
-    public static void main(String[] args){
-        System.out.println("Test");
+	
+	public HistogramSystem() {
+		
+	}
+    
+	public static void main(String[] args) throws IOException {
+        System.out.println("Test:");
+        HistogramSystem hs = new HistogramSystem();
+        hs.read();
     }
+	
+	public void read() throws IOException {
+		Path path = Paths.get("example.txt");
+		byte[] b = Files.readAllBytes(path);
+
+		for(int i = 0 ; i < b.length; i++) {
+		  // printing the characters one by one
+		  System.out.println((char) b[i]); 
+		}
+		ByteArrayInputStream bInput = new ByteArrayInputStream(b);
+	}
 
 }
---
+++ Test/src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by salomon on 25.04.17.
+ */
+public class HelloWorld {
+
+    public void hello() {
+        System.out.println("Hello World");
+    }
+}
