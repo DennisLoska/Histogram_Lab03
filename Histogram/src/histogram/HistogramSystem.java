18d5f6a9fefab8dc9321613245d1edef86499f09
disrupted <salomon.popp@gmail.com>
2017-04-27 00:11:42 +0200

    working document char count
    ignoring linebreaks

--- Histogram/src/histogram/HistogramSystem.java
+++ Histogram/src/histogram/HistogramSystem.java
@@ -5,6 +5,8 @@ import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Created by Dennis on 24.04.2017.
@@ -16,19 +18,30 @@ public class HistogramSystem {
 	}
     
 	public static void main(String[] args) throws IOException {
-        System.out.println("Test:");
         HistogramSystem hs = new HistogramSystem();
         hs.read();
     }
 	
 	public void read() throws IOException {
+		System.out.println("char – frequency");
 		Path path = Paths.get("example.txt");
 		byte[] b = Files.readAllBytes(path);
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
 
-		for(int i = 0 ; i < b.length; i++) {
-		  // printing the characters one by one
-		  System.out.println((char) b[i]); 
+		// read source & count character frequency
+		for (int i = 0 ; i < b.length; i++) {
+			if ((char) b[i] != '\n') { // ignore linebreaks
+				int frequency = 0;
+				try { frequency = map.get((char) b[i]); } catch (NullPointerException e) {}
+				map.put((char) b[i], frequency + 1);
+			}
 		}
+		
+		// print results
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+				System.out.println(entry.getKey() + " ––> " + entry.getValue() + "x");
+		}
+		
 		ByteArrayInputStream bInput = new ByteArrayInputStream(b);
 	}
 
--- Test/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by salomon on 25.04.17.
- */
-public class HelloWorld {
-
-    public void hello() {
-        System.out.println("Hello World");
-    }
-}
